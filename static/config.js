9ad8ea521585347d2e633c4051ca5ba18d48a73c
Bright2017 <www.601632305@qq.com>
2024-12-14 16:46:14 +0800

    图表新增滚动加载

--- static/config.js
+++ static/config.js
@@ -4,5 +4,6 @@
 var CONFIG = {
   baseUrl: 'https://www.bright2017.top',  // 请求地址 
   author: 'Bright', 
-  version: '1.0.0'  // 版本
+  version: '1.0.0',  // 版本
+  isProduction: true  // true 图片使用托管地址 false 使用静态图片
 };
