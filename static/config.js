904cc3d89d42e6f3dbc5d640d74d9ff4866b9d6f
yangliang <yangliang@phxg.cn>
2025-06-06 11:13:05 +0800

    更新根目录节点

--- static/config.js
+++
@@ -1,9 +0,0 @@
-/**
- * 全局配置
- */
-var CONFIG = {
-  baseUrl: 'https://www.bright2017.top',  // 请求地址 
-  author: 'Bright', 
-  version: '1.0.0',  // 版本
-  isProduction: true  // true 图片使用托管地址 false 使用静态图片
-};
